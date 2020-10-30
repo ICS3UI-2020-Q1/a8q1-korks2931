6c379019ac12c5fbf0a6482dd8053ef02ef5b15c
korks2931 <korks2931@wrdsb.ca>
2020-10-30 17:38:26 +0000

    finished a8q1

--- Main.java
+++ Main.java
@@ -4,7 +4,19 @@ import java.awt.event.*;
 
 public class Main implements Runnable, ActionListener{
 
-  // Class Variables  
+  // Class Variables 
+
+  JTextField nameInput;
+
+  JButton sayHelloButton;
+
+  JTextArea outputTextArea;
+  
+  JPanel mainPanel;
+
+
+  
+
   
 
 
@@ -18,7 +30,46 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+
+    // initialize the JTextFields 
+     nameInput = new JTextField();
+     // set the location and the size of the input fields 
+     nameInput.setBounds(200,200,300,30);
+     
+     //initialize the JButton
+     sayHelloButton = new JButton( "Say Hello");
+
+     // set the location and the size of the buttons 
+     sayHelloButton.setBounds (330,350,100,35);
+
+     //set the actionCommand 
+     sayHelloButton.setActionCommand("Say Hello");
+     // add the action listener to the button 
+     sayHelloButton.addActionListener(this);
+
+     //initialize the textarea
+     outputTextArea = new JTextArea();
+     outputTextArea.setBounds(200, 290,90,25);
+
+
+
+
+    // initialize the main Jpanel 
+    mainPanel = new JPanel();
+    // turn on the manual layouts 
+    mainPanel.setLayout(null);
+    // add the panel to the frame to see it 
+
+   // add the textfields to the panel
+   mainPanel.add(nameInput);
+   // add the buttons to the main panel 
+   mainPanel.add(sayHelloButton);
+   // add the text area to the panel 
+   mainPanel.add(outputTextArea);
  
+   // add the main panel to the screen
+   frame.add(mainPanel);
     
 
   }
@@ -28,6 +79,13 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+
+   if(command.equals("Say Hello")){
+     // say hello button was pressed 
+     String userName = nameInput.getText();
+     // say hello to the username
+     outputTextArea.setText("Hello " + userName);
+   }
   }
 
   // Main method to start our program
